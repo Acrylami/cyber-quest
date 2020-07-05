ec771dc51a500b18a413d4325801cf94dbd921f1
Arlyn <Acrylami@gmail.com>
2020-07-05 15:06:20 +0100

    Training
    Training icons and page layout added.
    Mostly just for demonstrating what the layout will look like. Probably will pull training from a training database so I can also pull their completed status and thus display the points in the right colour. Page nav to take to different training sections.
    
    Misc changes like footer.

--- cyber/routes.py
+++ cyber/routes.py
@@ -65,6 +65,18 @@ def training():
 def viper():
     return render_template('viper.html', title='Viper in the Dusk')
 
+@app.route("/viper-chapters")
+def viper_chapters():
+    return render_template('viper-chapters.html', title='Viper in the Dusk Chapters')
+
+@app.route("/viper-chapter-1")
+def viper_chapter_1():
+    return render_template('viper-chapter-1.html', title='Chapter 1')
+
+
+@app.route("/training-caesar")
+def training_caesar():
+    return render_template('training-caesar.html', title='Caesar Cipher Training')
 #Flask lab 3: implementation of Cart.  Needs 'session' import!
 # https://github.com/kkschick/ubermelon-shopping-app/blob/master/melons.py MELONS
 
